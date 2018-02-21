5b74c4643db156e86fe77d6ef06f2fe22264028b
Alexey Khrushch <alexeykhr@outlook.com>
2018-02-21 20:02:20 +0200

    Dashboard.vue - Some styles + facebook card

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,6 +5,7 @@
         <!-- FIXME: Images in data -->
         <img v-if="card.shortName === 'vk'" src="../static/vk/wallpaper.jpg">
         <img v-else-if="card.shortName === 'twitter'" src="../static/twitter/wallpaper.jpg">
+        <img v-else-if="card.shortName === 'facebook'" src="../static/facebook/wallpaper.jpg">
       </q-card-media>
       <q-card-title>
         {{ card.name }}
@@ -17,7 +18,7 @@
       <q-card-separator />
       <q-card-main>{{ card.content }}</q-card-main>
       <q-card-actions>
-        <q-btn flat class="full-width" color="primary" @click="goLink(card.shortName)">GO</q-btn>
+        <q-btn flat class="full-width" color="primary" @click="goLink(card.shortName)" :disabled="!card.canGo">GO</q-btn>
       </q-card-actions>
     </q-card>
   </div>
@@ -38,6 +39,7 @@
             shortName: 'vk',
             icon: 'fa-vk',
             domain: 'vk.com',
+            canGo: true,
             content: 'VK (VKontakte; Russian: ВКонта́кте, meaning InContact) is a Russian-based online social media and social networking service.'
           },
           {
@@ -45,12 +47,21 @@
             shortName: 'twitter',
             icon: 'fa-twitter',
             domain: 'twitter.com',
+            canGo: false,
             content: 'Twitter (/ˈtwɪtər/) is an online news and social networking service where users post and interact with messages, known as "tweets."'
+          },
+          {
+            name: 'Facebook',
+            shortName: 'facebook',
+            icon: 'fa-facebook',
+            domain: 'facebook.com',
+            canGo: false,
+            content: 'Facebook is an American online social media and social networking service company based in Menlo Park, California.'
           }
         ]
       }
     },
-    created () {
+    activated () {
       this.$store.dispatch('clearTemplateLeftSide')
       this.$store.dispatch('clearTemplateRightSide')
       this.$store.dispatch('clearTemplateSubTitle')
@@ -64,6 +75,17 @@
 </script>
 
 <style>
+  #general-dashboard {
+    margin: 0 auto;
+    text-align: center;
+    padding: 20px 0;
+  }
+  .q-card {
+    text-align: left;
+  }
+  .q-card-main {
+    height: 100px;
+  }
   .q-card-media > img {
     height: 180px;
   }
