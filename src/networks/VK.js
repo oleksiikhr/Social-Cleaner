6943292ee54ab55627638f01924b39551931f578
Alexey Khrusch <alexeykhr@outlook.com>
2018-05-28 18:55:47 +0300

    Updated VK class (sleep, fetchWallDetele)

--- src/networks/VK.js
+++ src/networks/VK.js
@@ -5,7 +5,6 @@ import { addLog } from '../heplers/logs'
 import store from '../store'
 import Vue from 'vue'
 
-// TODO Global process for block (send)
 const network = class VK {
   /* | -----------------------------------------------------------------------------
    * | API
@@ -38,58 +37,71 @@ const network = class VK {
   static async fetchWallGet (
     ownerId = store.state.vk.user.id,
     filter = 'all',
-    count = this.prototype.COUNT_GET_POSTS_BASIC,
-    offset = 0
+    count = this.prototype.COUNT_WALL_POSTS,
+    offset = 0,
+    sleepMin = 0,
+    sleepMax = sleepMin
   ) {
     const result = await this.send('wall.get', {
       owner_id: ownerId,
       filter: filter,
       count: count,
       offset: offset
-    })
+    }, { min: sleepMin, max: sleepMax })
+
+    return result
+  }
+  /**
+   * @see https://vk.com/dev/wall.delete
+   */
+  static async fetchWallDelete (postId, ownerId = store.state.vk.user.id, sleepMin = 0, sleepMax = sleepMin) {
+    const result = await this.send('wall.get', {
+      owner_id: ownerId,
+      post_id: postId
+    }, { min: sleepMin, max: sleepMax })
 
     return result
   }
   /**
    * @see https://vk.com/dev/users.get
    */
-  static async fetchUsersGet (userIds = store.state.vk.user.id, fields = '') {
+  static async fetchUsersGet (userIds = store.state.vk.user.id, fields = '', sleepMin = 0, sleepMax = sleepMin) {
     const result = await this.send('users.get', {
       user_ids: userIds,
       fields: fields
-    })
+    }, { min: sleepMin, max: sleepMax })
 
     return result
   }
   /**
    * @see https://vk.com/dev/groups.getById
    */
-  static async fetchGroupsGetById (groupIds, fields = '') {
+  static async fetchGroupsGetById (groupIds, fields = '', sleepMin = 0, sleepMax = sleepMin) {
     const result = await this.send('groups.getById', {
       group_ids: groupIds,
       fields: fields
-    })
+    }, { min: sleepMin, max: sleepMax })
 
     return result
   }
   /**
    * @see https://vk.com/dev/status.get
    */
-  static async fetchStatusGet (id = store.state.vk.user.id) {
+  static async fetchStatusGet (id = store.state.vk.user.id, sleepMin = 0, sleepMax = sleepMin) {
     const result = await this.send('status.get', {
       user_id: id
-    })
+    }, { min: sleepMin, max: sleepMax })
 
     return result
   }
   /**
    * @see https://vk.com/dev/status.set
    */
-  static async fetchStatusSet (text = '', group_id = null) {
+  static async fetchStatusSet (text = '', group_id = null, sleepMin = 0, sleepMax = sleepMin) {
     const result = await this.send('status.set', {
       text: text,
       group_id: group_id
-    })
+    }, { min: sleepMin, max: sleepMax })
 
     return result
   }
@@ -152,6 +164,7 @@ const network = class VK {
   }
 }
 
+// Basic, important information about the class
 network.prototype.name = 'Vkontakte'
 network.prototype.to = '/vk'
 network.prototype.domain = 'vk.com'
@@ -162,15 +175,17 @@ network.prototype.sections = [
   { name: 'vk.sections.status', to: 'vk-status', icon: icons.STATUS }
 ]
 
+// URL
 network.prototype.url = 'https://vk.com/'
 network.prototype.urlOauth = 'https://oauth.vk.com/authorize/'
 network.prototype.urlApi = 'https://api.vk.com/method/'
 network.prototype.urlRedirect = 'https://oauth.vk.com/blank.html'
 
+// API params
 network.prototype.clientId = 6244330
 network.prototype.version = '5.76'
 
-network.prototype.COUNT_GET_POSTS_BASIC = 25
-network.prototype.COUNT_GET_POSTS_MAX = 100
+// Information about methods
+network.prototype.COUNT_WALL_POSTS = 100
 
 export default network
