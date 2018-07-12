a0e3c830ede5f3f8f6e914c1320df5ad1df5abcb
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-12 08:17:19 +0300

    Fixed blink after open site

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <template>
-  <!--FIXME Blink after open site (appClass*)-->
-  <!--TODO Global Documentation for the user (how to use)-->
-  <div id="app" :class="appClass">
-    <loading-component />
-    <header-component />
-    <div id="content">
-      <keep-alive>
-        <router-view />
-      </keep-alive>
-    </div>
-    <footer-component />
+  <div id="app">
+    <keep-alive>
+      <router-view />
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import LoadingComponent from './components/parts/Loading'
-import FooterComponent from './components/footer/Index'
-import HeaderComponent from './components/header/Index'
-
 export default {
-  components: {
-    HeaderComponent, FooterComponent, LoadingComponent
-  },
-  computed: {
-    appClass () {
-      return this.$store.state.template.appClass
-    }
-  }
 }
 </script>
