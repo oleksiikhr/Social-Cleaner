4744bb8e8a55274acf7dd00aa3dc384581d75227
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-12 08:16:29 +0300

    Update routes (use layouts)

--- src/router/index.js
+++ src/router/index.js
@@ -17,62 +17,76 @@ const router = new Router({
   scrollBehavior: () => ({ y: 0 }),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: load('Home')
-    },
-    {
-      path: '/explore',
-      name: 'explore',
-      component: load('Explore')
-    },
-    {
-      path: '/logs',
-      name: 'logs',
-      component: load('Logs')
-    },
-    {
-      path: '/vk',
-      component: load('vk/Home'),
+      path: '',
+      name: 'layout-landing',
+      component: load('layouts/Landing'),
       children: [
         {
-          path: '',
-          name: 'vk',
-          component: load('vk/Token'),
-          meta: {
-            name: 'vk.sections.token',
-            icon: icons.TOKEN
-          }
+          path: '/',
+          name: 'home',
+          component: load('Home')
+        }
+      ]
+    },
+    {
+      path: '',
+      name: 'layout-default',
+      component: load('layouts/Default'),
+      children: [
+        {
+          path: '/explore',
+          name: 'explore',
+          component: load('Explore')
         },
         {
-          path: 'wall',
-          name: 'vk-wall',
-          component: load('vk/Wall'),
-          meta: {
-            vk: true,
-            name: 'vk.sections.wall',
-            icon: icons.WALL
-          }
+          path: '/logs',
+          name: 'logs',
+          component: load('Logs')
         },
         {
-          path: 'status',
-          name: 'vk-status',
-          component: load('vk/Status'),
-          meta: {
-            vk: true,
-            name: 'vk.sections.status',
-            icon: icons.STATUS
-          }
-        },
-        {
-          path: 'docs',
-          name: 'vk-docs',
-          component: load('vk/Docs'),
-          meta: {
-            vk: true,
-            name: 'vk.sections.docs',
-            icon: icons.DOCS
-          }
+          path: '/vk',
+          component: load('vk/Home'),
+          children: [
+            {
+              path: '',
+              name: 'vk',
+              component: load('vk/Token'),
+              meta: {
+                name: 'vk.sections.token',
+                icon: icons.TOKEN
+              }
+            },
+            {
+              path: 'wall',
+              name: 'vk-wall',
+              component: load('vk/Wall'),
+              meta: {
+                vk: true,
+                name: 'vk.sections.wall',
+                icon: icons.WALL
+              }
+            },
+            {
+              path: 'status',
+              name: 'vk-status',
+              component: load('vk/Status'),
+              meta: {
+                vk: true,
+                name: 'vk.sections.status',
+                icon: icons.STATUS
+              }
+            },
+            {
+              path: 'docs',
+              name: 'vk-docs',
+              component: load('vk/Docs'),
+              meta: {
+                vk: true,
+                name: 'vk.sections.docs',
+                icon: icons.DOCS
+              }
+            }
+          ]
         }
       ]
     }
