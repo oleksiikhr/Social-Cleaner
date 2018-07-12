9d35523261629984dfd4e809925a0dcd8d394935
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-12 08:15:57 +0300

    Add layouts (landing, default)

---
+++ src/components/layouts/Default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="default-layout layout">
+    <loading-component />
+    <header-component />
+    <div id="content">
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </div>
+    <footer-component />
+  </div>
+</template>
+
+<script>
+import LoadingComponent from '../../components/parts/Loading'
+import FooterComponent from '../../components/footer/Index'
+import HeaderComponent from '../../components/header/Index'
+
+export default {
+  components: {
+    HeaderComponent, FooterComponent, LoadingComponent
+  }
+}
+</script>
