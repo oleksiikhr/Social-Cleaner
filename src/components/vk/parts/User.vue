e0be325193c38c6621e0576f96c691c01185359c
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-13 08:22:29 +0300

    vk/parts/User - Update design

--- src/components/vk/parts/User.vue
+++ src/components/vk/parts/User.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="user">
-    <div class="user__image">
-      <img :src="user.photo_100" :alt="name">
+  <a :href="linkUser" target="_blank" rel="noopener" class="user">
+    <div class="user__left">
+      <div class="user__image">
+        <img :src="user.photo_100" :alt="name">
+      </div>
     </div>
-    <div class="user-info">
-      <div class="user__id">
-        {{ user.id }}
-      </div>
-      <div class="user__name">
-        {{ name }}
-      </div>
-      <div class="user__counters">
-        <div :class="`user__counter ${attribute}`" v-for="(attribute, key) in atributes" :key="key">
-          <!--TODO Design-->
-          <span class="user__counter-name">{{ $t(`vk.scope.${attribute}`)}}:</span>
-          <span class="user__counter-value">{{ user.counters[attribute] }}</span>
+    <div class="user__right">
+      <div class="user__name">{{ name }}</div>
+      <div class="user-info">
+        <div class="user__counters">
+          <div :class="`user__counter ${attribute}`" v-for="(attribute, key) in atributes" :key="key">
+            <span class="user__counter-name">{{ $t(`vk.scope.${attribute}`)}}</span>
+            <span class="user__counter-value">{{ user.counters[attribute] }}</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </a>
 </template>
 
 <script>
+import VK from '../../../media/VK'
+
 export default {
   props: {
     user: {
@@ -40,6 +40,9 @@ export default {
   computed: {
     name () {
       return `${this.user.last_name} ${this.user.first_name}`
+    },
+    linkUser () {
+      return VK.getLinkUser()
     }
   }
 }
@@ -47,15 +50,74 @@ export default {
 
 <style lang="scss" scoped>
 .user {
-  max-width: 500px;
-  margin: 0 auto;
   display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 700px;
+  margin: 0 auto;
+  border: 1px solid #e7e7e7;
+  background: #fbfbfb;
+  padding: 20px;
+  color: #333;
+  transition: .3s;
+  &:hover {
+    background: #585f6c;
+    color: rgba(255, 255, 255, .87);
+  }
+}
+
+.user__left {
+  line-height: 0;
+  margin-right: 20px;
+}
+
+.user__name {
+  font-weight: bold;
+  font-size: 1rem;
+  text-align: center;
+  border-bottom: 1px solid #e7e7e7;
+  margin: 0 20px 5px;
+  padding-bottom: 5px;
 }
 
 .user__image {
   > img {
     max-width: 100px;
     max-height: 100px;
+  }
+}
+
+.user__counters {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.user__counter {
+  display: flex;
+  flex-direction: column;
+  margin: 5px 9px;
+  text-align: center;
+  &-name {
+    font-weight: bold;
+    font-size: .8rem;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .user {
+    flex-direction: column;
+  }
+
+  .user__left {
+    margin-bottom: 10px;
+    margin-right: 0;
+  }
+
+  .user__name {
+    margin-bottom: 10px;
+    padding-bottom: 10px;
   }
 }
 </style>
