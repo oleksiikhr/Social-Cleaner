19c6426d29d5a96c6895e87123e5049974868554
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-22 13:44:53 +0300

    VK class - add comments

--- src/media/VK.js
+++ src/media/VK.js
@@ -5,10 +5,12 @@ import router from '../router'
 import store from '../store'
 import Vue from 'vue'
 
+/** @see router - Position in array */
 const ROUTE_INDEX = [1, 2]
 
 const DEFAULT_LANG = 'en'
 
+/** @see https://vk.com/dev/api_requests - lang param */
 const LIST_OF_LANG = [
   'ru', 'uk', 'be', 'en', 'es', 'fi', 'de', 'it'
 ]
@@ -19,16 +21,42 @@ const media = class VK {
    * | -----------------------------------------------------------------------------
    * |
    */
+  /**
+   * Run the process, clear the results (if any)
+   *
+   * @param component
+   */
   static defaultStart (component) {
     store.commit('START_PROCESS', 'vk')
     component.loading = true
     component.result = []
   }
+  /**
+   * Stop the process. Remove the global "Stop" button
+   *
+   * @param component
+   */
   static defaultStop (component) {
     store.commit('STOP_PROCESS', 'vk')
     store.commit('CLEAR_CANCEL', 'vk')
     component.loading = false
   }
+  /**
+   * Process:
+   * Get the maximum number of records. Pass through each record and check.
+   * If the check is successful - delete. No - skip.
+   * We get a trace. a lot of posts and go through again.
+   * If there are any errors or items are empty - we stop.
+   *
+   * NOTE: Callbacks are required in the component!
+   * callbackGet - get the data from the server.
+   * callbackCheck - Checking the recording of filters.
+   * callbackDelete - send a request to the server for deletion.
+   *
+   * @param component
+   * @param {number} maxCountApi
+   * @return {Promise<void>}
+   */
   static async doStartDefault (component, maxCountApi) {
     this.defaultStart(component)
 
@@ -59,6 +87,19 @@ const media = class VK {
 
     return this.defaultStop(component)
   }
+  /**
+   * Process:
+   * Get the maximum number of records. Pass through each record and check.
+   * Check each entry and resend requests to the server.
+   *
+   * NOTE: Callbacks are required in the component!
+   * callbackGet - get the data from the server.
+   * callbackCheck - Checking the recording of filters.
+   *
+   * @param component
+   * @param {number} maxCountApi
+   * @return {Promise<void>}
+   */
   static async doPreviewDefault (component, maxCountApi) {
     this.defaultStart(component)
 
@@ -67,7 +108,6 @@ const media = class VK {
     for (let i = 0; i < countLoop; i++) {
       const offset = i * maxCountApi
       const leftItems = component.main.count.max - offset
-
       const res = await component.callbackGet(leftItems > maxCountApi ? maxCountApi : leftItems, offset)
 
       if (res.ok && res.body.response && res.body.response.items.length) {
