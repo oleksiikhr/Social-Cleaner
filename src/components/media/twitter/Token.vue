fff446a0158303bba2e4e1ed6511fa56b3d60328
Alexey Khrusch <alexeykhr@outlook.com>
2018-08-05 18:45:30 +0300

    Add basic 2 components for Twitter (Home, Token)

---
+++ src/components/media/twitter/Token.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="token">
+    Token
+  </div>
+</template>
+
+<script>
+import Twitter from '../../../media/Twitter'
+
+export default {
+  data () {
+    return {
+      Twitter
+    }
+  },
+  computed: {
+    user () {
+      return this.$store.state.media.twitter.user
+    },
+    isAuth () {
+      return this.$store.state.media.twitter.isAuth
+    },
+    process () {
+      return this.$store.state.media.twitter.process
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
