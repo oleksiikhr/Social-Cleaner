f17c91cf4c4e4be298f8aaf7d7466ca067d0dbc5
Alexey Khrusch <alexeykhr@outlook.com>
2018-08-09 23:22:18 +0300

    Improve grab networks

--- src/networks/index.js
+++ src/networks/index.js
@@ -1,23 +1,36 @@
+// List of all social networks
+// TODO Auto get of all social networks
 const networks = ['VK', 'Github', 'Twitter']
+
+// Folders to be imported into storage (index.js file)
 const folders = ['class', 'store', 'router', 'locale', 'scss']
 
+// Write all the data here
 const storage = {}
 
+const loadClass = (network) => {
+  return () => import(`./${network}/${network}.js`).default.prototype
+}
+
+const loadFolder = (network, folder) => {
+  return () => import(`./${network}/${folder}`).default
+}
+
+// Initialize array to every folder
 folders.forEach(folder => {
   storage[folder] = []
 })
 
 networks.forEach(network => {
   // Import prototype of every class
-  storage['class'].push(require(`./${network}/${network}.js`).default.prototype)
+  storage['class'].push(loadClass(network))
 
+  // Import other folders
   folders.forEach(folder => {
     try {
-      storage[folder].push(require(`./${network}/${folder}`).default)
+      storage[folder].push(loadFolder(network, folder))
     } catch (e) { }
   })
 })
-
-console.log(storage)
 
 export default storage
