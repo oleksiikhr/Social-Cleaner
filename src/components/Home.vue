880afcf055e0fd9f0cf1eee10ba6538d58daedcc
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-16 11:42:55 +0300

    Add Home.vue, include files to main.js

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Home.vue</div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    console.log('Here')
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
+import router from './router'
+import store from './store'
 import App from './App.vue'
+import Vue from 'vue'
 
+// Styles
+import './styles/index.scss'
+
+// Hide message
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
