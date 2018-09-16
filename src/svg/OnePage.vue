1c596ffa6e68f310cfaf2c6146dc002c5c513c91
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-16 21:40:49 +0300

    Add svg files

---
+++ src/svg/OnePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+       width="50" height="50"
+       viewBox="0 0 50 50">
+    <g>
+      <path d="M 7 2 L 7 48 L 43 48 L 43 15.410156 L 29.183594 2 Z M 9 4 L 28 4 L 28 17 L 41 17 L 41 46 L 9 46 Z M 30 5.578125 L 39.707031 15 L 30 15 Z M 25 23 C 25 23 24.851563 25.347656 22 25.347656 L 22 27 L 25 27 L 25 36 L 27 36 L 27 23 Z " />
+    </g>
+  </svg>
+</template>
---
+++ src/svg/WebDesign.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+       width="50" height="50"
+       viewBox="0 0 50 50">
+    <path style="line-height:normal;text-indent:0;text-align:start;text-decoration-line:none;text-decoration-style:solid;text-decoration-color:#000;text-transform:none;block-progression:tb;isolation:auto;mix-blend-mode:normal"
+          d="M 5 4 A 1.0001 1.0001 0 0 0 4 5 L 4 15 A 1.0001 1.0001 0 0 0 5 16 L 45 16 A 1.0001 1.0001 0 0 0 46 15 L 46 5 A 1.0001 1.0001 0 0 0 45 4 L 5 4 z M 6 6 L 44 6 L 44 14 L 6 14 L 6 6 z M 10 9 A 1 1 0 0 0 9 10 A 1 1 0 0 0 10 11 A 1 1 0 0 0 11 10 A 1 1 0 0 0 10 9 z M 14 9 A 1 1 0 0 0 13 10 A 1 1 0 0 0 14 11 A 1 1 0 0 0 15 10 A 1 1 0 0 0 14 9 z M 18 9 A 1 1 0 0 0 17 10 A 1 1 0 0 0 18 11 A 1 1 0 0 0 19 10 A 1 1 0 0 0 18 9 z M 4 19 L 4 45 A 1.0001 1.0001 0 0 0 5 46 L 45 46 A 1.0001 1.0001 0 0 0 46 45 L 46 19 L 44 19 L 44 44 L 6 44 L 6 19 L 4 19 z M 10 19 A 1.0001 1.0001 0 0 0 9 20 L 9 40 A 1.0001 1.0001 0 0 0 10 41 L 23 41 A 1.0001 1.0001 0 0 0 24 40 L 24 20 A 1.0001 1.0001 0 0 0 23 19 L 10 19 z M 27 19 L 27 21 L 41 21 L 41 19 L 27 19 z M 11 21 L 22 21 L 22 39 L 11 39 L 11 21 z M 27 24 L 27 26 L 41 26 L 41 24 L 27 24 z M 27 29 L 27 31 L 41 31 L 41 29 L 27 29 z M 27 34 L 27 36 L 41 36 L 41 34 L 27 34 z M 27 39 L 27 41 L 41 41 L 41 39 L 27 39 z" />
+  </svg>
+</template>
